aa6f252a2ca6f97890a35d4c5d09d193bc272e38
KonohaDerek <peter890701@gmail.com>
2021-01-10 19:07:18 +0800

    加入tg 機器人測試

---
+++ FundraisingSplit.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Telegram.Bot;
+using System.Text.Json;
+using Telegram.Bot.Args;
+
+namespace FundraisingSplitConsole
+{
+
+  class Program
+  {
+
+    static ITelegramBotClient botClient;
+
+
+    static void Main(string[] args)
+    {
+      var token = "910140411:AAGLYKHlLpm0rUTzgrPbJNL2gnv5sIvVvVA";
+      botClient = new TelegramBotClient(token);
+      var me = botClient.GetMeAsync().Result;
+
+      Console.WriteLine(
+          $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+      );
+      botClient.OnMessage += Bot_OnMessage;
+      botClient.StartReceiving();
+
+      Console.WriteLine("Press any key to exit");
+      Console.ReadKey();
+
+      botClient.StopReceiving();
+    }
+
+    static async void Bot_OnMessage(object sender, MessageEventArgs e)
+    {
+      if (e.Message.Text != null)
+      {
+        Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+        await botClient.SendTextMessageAsync(
+          chatId: e.Message.Chat,
+          text: "You said:\n" + e.Message.Text
+        );
+      }
+    }
+
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FundraisingSplit
 集資分帳處理系統
+
+
+# 使用TG 作為通知處理
+## TG 機器人教學文件
+https://telegrambots.github.io/book/1/example-bot.html
